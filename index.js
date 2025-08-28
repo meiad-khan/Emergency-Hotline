2064808a120f0c5ab8f20e1901d07d0809c03d5a
meiad-khan <meiadkhan615@gmail.com>
2025-08-28 09:47:34 +0600

    made heart dynamic

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log('hello meiad from index.js');
+
+let sum=0;
+const countLove = document.getElementsByClassName('count-love');
+for(const count of countLove){
+  count.addEventListener('mouseover', function(){
+    count.style.color = 'red';
+  })
+  count.addEventListener('click',function(){
+    sum++;
+    const putCountLove = parseInt(document.getElementById('put-count-love').innerText);
+    
+   document.getElementById('put-count-love').innerText = sum;
+   })
+   count.addEventListener('mouseout',function(){
+    count.style.color = 'black';
+   })
+}
